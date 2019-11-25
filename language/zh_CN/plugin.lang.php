48a4d814e2c4198b423122bcc38bd672867fc757
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-11-25 06:07:07 +0100

    Update zh_CN, thanks to Arnie97

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -20,19 +20,19 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['%s says on %s :'] = '%s 留言 %s :';
-$lang['* : mandatory fields'] = '* : 必填字段';
+$lang['%s says on %s :'] = '%s 在页面 %s 上留言：';
+$lang['* : mandatory fields'] = '*: 必填字段';
 $lang['Activate rating'] = '启用评分';
 $lang['GuestBook'] = '留言本';
 $lang['Please enter your username'] = '请输入您的用户名';
 $lang['Sign the guestbook'] = '我要留言';
-$lang['There are %d messages'] = '有%d个留言';
+$lang['There are %d messages'] = '共有 %d 条留言';
 $lang['Website'] = '网站';
 $lang['invalid website address'] = '无效网站地址';
 $lang['not publicly visible'] = '不公开';
-$lang['Please enter a message'] = '请输入信息';
-$lang['Please enter your e-mail'] = '请输入你的电子邮件';
-$lang['Guests (not registered) permissions :'] = '游客(未注册)权限';
+$lang['Please enter a message'] = '请输入留言内容';
+$lang['Please enter your e-mail'] = '请输入你的电子邮箱';
+$lang['Guests (not registered) permissions :'] = '游客（未注册用户）权限';
 $lang['add comment'] = '添加评论';
 $lang['view comments'] = '查看评论';
 $lang['Rate'] = '评分';
